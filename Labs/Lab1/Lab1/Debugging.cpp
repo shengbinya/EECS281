8bf676b16819347beb11ed92d9a79abd02171d15
mmartin4972 <mmartin4972@gmail.com>
2020-09-11 17:04:59 -0400

    Finished Lab 1

---
+++ Labs/Lab1/Lab1/Debugging.cpp
@@ -0,0 +1,10 @@
+﻿#include <vector>
+int *get_some_ints() {
+	std::vector<int> ints = { 1,2,3,4,5};
+	return ints.data();
+}
+int main() {
+	int* some_ints = get_some_ints();
+	delete[] some_ints;
+	return 0;
+}

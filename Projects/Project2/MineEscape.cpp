7b789911377227b28a57be5941c2aa7c1d0fb557
mmartin4972 <mmartin4972@gmail.com>
2020-10-08 01:54:45 -0400

    Added command line functionality

--- Projects/Project2/MineEscape.cpp
+++ Projects/Project2/MineEscape.cpp
@@ -6,6 +6,8 @@
 #include <deque>
 #include <queue>
 #include "P2random.h"
+#include <set>
+#include <iomanip>
 
 using namespace std;
 
@@ -25,6 +27,23 @@ public:
 
 struct tileComp {
     bool operator()(const tile& a, const tile& b) {
+        if (a.rubble > b.rubble)
+            return true;
+        else if (a.rubble < b.rubble)
+            return false;
+        else if (a.col > b.col)
+            return true;
+        else if (a.col < b.col)
+            return false;
+        else if (a.row > b.col)
+            return true;
+        else
+            return false;
+    }
+};
+
+struct tileCompOp {
+    bool operator()(const tile& b, const tile& a) {
         if (a.rubble > b.rubble)
             return true;
         else if (a.rubble < b.rubble)
@@ -60,20 +79,30 @@ public:
 
     void setEdge(int row, int col);
 
+    void insert(int);
+
+    void printMedian();
+
+    void printStats();
+
 private:
 
     //Underlying Data Structures
 	vector<vector<tile>> layout;
     priority_queue<tile, vector<tile>, tileComp> primaryQueue;
     priority_queue<tile, vector<tile>, tileComp> TNTQueue;
+    vector<int> median;
+    vector<tile> discard;
 
     //Initial Variables
     pair<unsigned int, unsigned int> start = { 0,0 };
+    unsigned int totalRubble = 0;
+    unsigned int totalTiles = 0;
 
     //Command Line Args
     int firstCleared = -1;
-    bool median = false;
-    bool verbose = false;
+    bool m = false;
+    bool v = false;
     tile finalEdge;
     
 
@@ -99,15 +128,15 @@ void mine::get_options(int argc, char** argv) {
             break;
 
         case 'm':
-            median = true;
+            m = true;
             break;
 
         case 'v':
-            verbose = true;
+            v = true;
             break;
 
         case 'h':
-            std::cerr << "This program reads in a mine from a file.\n"
+           cout << "This program reads in a mine from a file.\n"
                 << "It finds the easiest clear path through the mine\n"
                 << "and can generate output to indicate this path\n"
                 << "according to the provided arguemnts."
@@ -132,7 +161,7 @@ void mine::readMine() {
     cin >> mode;
     //Check if mode is correct
     if (mode != "R" && mode != "M") {
-        cerr << "Invalid input mode\n";
+        cout << "Invalid input mode\n";
         exit(1);
     }
 
@@ -147,14 +176,14 @@ void mine::readMine() {
     start.first = stoi(in);
     //Check if start point is out of range
     if (start.first >= layout.size()) {
-        cerr << "Invalid starting row\n";
+        cout << "Invalid starting row\n";
         exit(1);
     }
 
     cin >> in;
     start.second = stoi(in);
     if (start.second > layout.size()) {
-        cerr << "Invalid starting column\n";
+        cout << "Invalid starting column\n";
         exit(1);
     }
 
@@ -188,6 +217,26 @@ void mine::readMine() {
 
 }
 
+void mine::insert(int val) {
+    auto it = lower_bound(median.begin(), median.end(), val);
+    median.insert(it, val);
+}
+
+void mine::printMedian() {
+    cout << "Median difficulty of clearing rubble is: ";
+    cout << std::fixed << std::setprecision(2);
+    if (median.size() % 2 == 0)
+        if (median.size() != 1) {
+            double step = (median[median.size() / 2] + median[median.size() / 2 - 1]);
+            step = step / 2;
+            cout << step << "\n";
+        }
+        else
+            cout << double(median.at(0)) << "\n";
+    else
+        cout << double(median[median.size() / 2]) << "\n";
+}
+
 void mine::writeMine() {
     for (unsigned int i = 0; i < layout.size(); ++i) {
         for (unsigned int j = 0; j < layout.size(); ++j) {
@@ -197,6 +246,46 @@ void mine::writeMine() {
     }
 }
 
+void mine::printStats() {
+    
+    cout << "First tiles cleared:\n";
+    for (int i = 0; i < firstCleared && i < int(discard.size()); i++) {
+        if (discard[i].rubble != -1)
+            cout << discard[i].rubble << " at [" << discard[i].row << "," << discard[i].col << "]\n";
+        else
+            cout << "TNT at [" << discard[i].row << "," << discard[i].col << "]\n";
+    }
+    
+    cout << "Last tiles cleared:\n";
+    for (int i = int(discard.size()) - 1; i > int(discard.size()) - firstCleared && i > -1; --i) {
+        if (discard[i].rubble != -1)
+            cout << discard[i].rubble << " at [" << discard[i].row << "," << discard[i].col << "]\n";
+        else
+            cout << "TNT at [" << discard[i].row << "," << discard[i].col << "]\n";
+    }
+
+    priority_queue<tile, vector<tile>, tileComp> statsQueue (discard.begin(), discard.end());
+    cout << "Easiest tiles cleared:\n";
+    for (int i = 0; i < firstCleared && i < int(discard.size()); i++) {
+        if (statsQueue.top().rubble != -1)
+            cout << statsQueue.top().rubble << " at [" << statsQueue.top().row << "," << statsQueue.top().col << "]\n";
+        else
+            cout << "TNT at [" << statsQueue.top().row << "," << statsQueue.top().col << "]\n";
+        statsQueue.pop();
+    }
+
+    priority_queue<tile, vector<tile>, tileCompOp> statsQueueBack(discard.begin(), discard.end());
+    cout << "Hardest tiles cleared:\n";
+    for (int i = 0; i < firstCleared && i < int(discard.size()); i++) {
+        if (statsQueueBack.top().rubble != -1)
+            cout << statsQueueBack.top().rubble << " at [" << statsQueueBack.top().row << "," << statsQueueBack.top().col << "]\n";
+        else
+            cout << "TNT at [" << statsQueueBack.top().row << "," << statsQueueBack.top().col << "]\n";
+        statsQueueBack.pop();
+    }
+
+    
+}
 
 bool mine::edge(const tile& current) {
     if (current.col != 0 && current.row != 0 &&
@@ -217,25 +306,25 @@ void mine::discover(const tile& current, priority_queue<tile, vector<tile>, tile
     unsigned int col = current.col;
 
     if (row != 0) {
-        if (layout[row - 1][col].rubble != -2)
+        if (layout[row - 1][col].rubble != 0)
             pq.push(layout[current.row - 1][current.col]);
     }
     else
         setEdge(row, col);
     if (row != layout.size()-1) {
-        if (layout[row + 1][col].rubble != -2)
+        if (layout[row + 1][col].rubble != 0)
             pq.push(layout[current.row + 1][current.col]);
     }
     else
         setEdge(row, col);
     if (col != 0) {
-        if (layout[row][col - 1].rubble != -2)
+        if (layout[row][col - 1].rubble != 0)
             pq.push(layout[current.row][current.col - 1]);
      }
     else
         setEdge(row, col);
     if (col != layout.size()-1) {
-        if (layout[row][col + 1].rubble != -2)
+        if (layout[row][col + 1].rubble != 0)
             pq.push(layout[current.row][current.col + 1]);
     }
     else
@@ -266,19 +355,30 @@ void mine::breakout() {
 
                 //Check if TNT
                 if (top.rubble == -1) {
-                    cout << "TNT explosion at [" << top.row << "," << top.col << "]!\n";
+                    if(v)
+                        cout << "TNT explosion at [" << top.row << "," << top.col << "]!\n";
+                    if (firstCleared != -1)
+                        discard.push_back(top);
                     discover(top, TNTQueue);
-                    layout[top.row][top.col].rubble = -2;
+                    layout[top.row][top.col].rubble = 0;
                     TNTQueue.pop();
                 }
 
                 //If not TNT Surroundings to Main Queue
                 else {
-                    if(top.rubble != 0)
+                    if(v)
                         cout << "Cleared by TNT: " << top.rubble << " at [" << top.row << "," << top.col << "]\n";
+                    if (m) {
+                        insert(top.rubble);
+                        printMedian();
+                    }
+                    if (firstCleared != -1)
+                        discard.push_back(top);
                     TNTQueue.pop();
                     discover(top, primaryQueue);
-                    layout[top.row][top.col].rubble = -2;
+                    ++totalTiles;
+                    totalRubble += layout[top.row][top.col].rubble;
+                    layout[top.row][top.col].rubble = 0;
                 }
 
             }
@@ -286,18 +386,32 @@ void mine::breakout() {
 
         //If not TNT then just clear it
         else {
-            cout << "Cleared " << current.rubble << " at [" << current.row << "," << current.col << "]\n";
+            if(v)
+                cout << "Cleared " << current.rubble << " at [" << current.row << "," << current.col << "]\n";
+            if (m) {
+                insert(current.rubble);
+                printMedian();
+            } 
+            if (firstCleared != -1)
+                discard.push_back(current);
             primaryQueue.pop();
             discover(current, primaryQueue);
-            layout[current.row][current.col].rubble = -2;
+            ++totalTiles;
+            totalRubble += layout[current.row][current.col].rubble;
+            layout[current.row][current.col].rubble = 0;
         }
     }
 
+    cout << "Cleared " << totalTiles << " tiles containing " << totalRubble << " rubble and escaped.\n";
+    if (firstCleared != -1)
+        printStats();
 }
 
 int main(int argc, char** argv) {
+    cout << std::fixed << std::setprecision(2);
 	mine jellystone;
 	jellystone.get_options(argc, argv);
     jellystone.readMine();
+    //jellystone.writeMine();
     jellystone.breakout();
 }
